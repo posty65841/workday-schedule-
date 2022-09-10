5f4bf0b8872781791e1b2ad1d92fd5ee5c6f6b30
austin <posty65841@gmail.com>
2022-09-10 14:02:18 -0500

    local storage issues

--- script.js
+++ script.js
@@ -24,15 +24,19 @@ function setTime() {
 
 
   // try to save  just one text  to local storage 
-  // local storage = string from data-id!!!
+ 
   //only save for 1 box at a time via event.target // event delgation- text area 
-
+//if present time color = red
+//if past color = white
+// if future color = green 
   
 
   $(".saveBtn").on("click", function(){
     let text = $(this).siblings('textarea').val();
     let time = $(this).siblings('div').text();
     localStorage.setItem(time, text)
+    
     console.log(text)
     })
     
+
