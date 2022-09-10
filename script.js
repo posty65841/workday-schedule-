aab14df94c1f05575d4c8766116e831cfd08b36f
austin <posty65841@gmail.com>
2022-09-09 21:12:35 -0500

    most recent commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
-
+let container = document.querySelector(".container")
+let button = document.querySelector(".saveBtn")
 
 
 let time = moment();
@@ -21,3 +21,20 @@ function setTime() {
 
   setTime();
 
+
+
+  // try to save  just one text  to local storage 
+  // local storage = string from data-id!!!
+  //only save for 1 box at a time via event.target // event delgation- text area 
+
+  
+
+container.addEventListener("click", function(event){
+
+    if(event.target.matches(".saveBtn")){
+        console.log(".container")
+    }
+
+
+
+} )
